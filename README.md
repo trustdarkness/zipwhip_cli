329f223408c11ee08edf89445b5e3595c387dcc5
mt <mt@trustdarkness.com>
2014-09-13 08:54:31 -0500

    Initial add of working zipwhip cli.

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 zipwhip_cli
 ===========
+
+This is a command line interface to the ZipWhip cloud texting service.
+
+A short backstory:  I hate typing on my phone.  Thus, when Google Voice
+came out, I was happy to have an alternative to text via my computer.
+Later, I became a privacy nut and pulled my data out of Google's services
+but I found I really missed the functionality of Google Voice for texting.
+
+ZipWhip doesn't have all of that functionality, but it has some of it, and
+though there is no privacy inherint in ZipWhip's service, I don't need 
+to lock into a whole ecosystem to use it like I do Google Voice. 
+
+Though I give kudos to ZipWhip for making a linux client, they 
+unfortunately made it only for Ubuntu and did not release the source.
+I used the web interface on Fedora for some time, but grew tired of having
+to use up an extra tab just for that.  Thankfully, the folks at ZipWhip
+have a great API.
+
+So, I threw together this very basic command line interface.  It currently
+has ZERO bells and whistles.  It will allow you to view the last message
+in every conversation in your texting "inbox" and it will allow you to send
+text messages via a phone number.
+
+Usage is pretty simple:
+$ ./zwcli.py
+Enter zipwhip number: 3125555555
+Enter password: 
+Would you like to save this info? 
+THIS IS NOT SECURE and will allow anyone with access to this
+account on your computer the ability to send and recieve 
+messages via your ZipWhip account
+Save? y/n 
+ySelect one of the following options:
+    1. See recent conversations
+    2. Send a text
+ 
+selection: 1
+New? | From:      | Msg: 
+     | 651XXXXXXX | Not 4 hours big dude! Maybe 20 min with a real connection.
+     | 312XXXXXXX | testing 
+     | 773XXXXXXX | again.
+     | 312XXXXXXX | Sure
+     | 630XXXXXXX | no problem... we'll talk soon.
+     | 773XXXXXXX | -=call=-  Outgoing Phone Call Duration: 60 seconds
+     | 630XXXXXXX | -=call=-  Outgoing Phone Call Duration: 14 seconds
+     | 805XXXXXXX | -=call=-  Outgoing Phone Call Duration: 20 seconds
+     | 630XXXXXXX | -=call=-  Missed Phone Call
+     | 630XXXXXXX | Thanks
+     | 773XXXXXXX |  sorry for the delayed response... didn't see your message 
+     |            |  right away
+     |            |  
+     |            |  
+     |      20000 |  .signup verify 9309 Zipwhip installation complete! To 
+     |            |  send, receive, and view all your texts complete 
+     |            |  registration from your computer at zipwhip.com 
+     |            |  
+     |        456 |  Free T-Mobile Msg: $60.00 has been added to your account. 
+     |            |  Your new balance is $61.00. Thanks for refilling! 
+     |            |  
+     | 952XXXXXXX |  i think we'll have a better idea about january
+     | 708XXXXXXX | Nooo
+     | 773XXXXXXX | Another test
+     | 7084850010 | -=call=-  Incoming Phone Call Duration: 29 seconds
+  <more> 
+
+
+
++++ snip +++
+
+$ ./zwcli.py
+You've successfully logged in.
+Select one of the following options:
+    1. See recent conversations
+    2. Send a text
+ 
+selection: 2
+Phone Number to Text: 773XXXXXXX
+Message to send: I hate white rabbits!
+ 
+Getting ready to send
+ 
+     I hate white rabbits!
+ 
+  to: 773XXXXXXX
+Confirm? y/n y
+Message sent successfully!
+
+
+TODOS:
+======
+
+I'm open to feature requests, but the next things *I* would at least like to
+work on implementing:
+
+- ncurses ui (similar to mcabber)
+- encryption using socialist millionaire
+
+If you've stumbled across this and use it, please let me know!
---
+++ zwcli.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python3
+import subprocess
+import getpass
+import pickle
+import sys
+import os
+try:
+  import WebCalls
+except:
+  print("Webcalls.py from the ZipWhip python API must be in your PYTHON_PATH")
+  sys.exit(0)
+
+__author__ = "voytek@trustdarkness.com"
+__email__ = "voytek@trustdarkness.com"
+__license__ = "GPL2"
+
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+home = os.path.expanduser("~")
+
+SETTINGS_DIR = os.path.join(home, ".zwcli")
+SETTINGS_FILE = os.path.join(home, ".zwcli", "settings")
+zwc = WebCalls.WebCalls()
+
+def authenticate():
+  """
+  Authenticate to ZipWhip and get a session key.  Offer to save user
+  credentials locally (insecurely, for now).
+
+  Returns:
+    Session Key to be used in future operations.  Also optionally
+    saves login information to ~/.zw/settings
+  """
+  autologin = False
+  try:
+    with open(SETTINGS_FILE, 'rb') as f: 
+      u, p = pickle.load(f)
+      autologin = True
+  except:
+    u = input("Enter zipwhip number: ")
+    p = getpass.getpass("Enter password: ")
+  r = zwc.user_login(u,p)
+  s = r.get("response")
+  if r.get("success"):
+    print("You've successfully logged in.")
+    if not autologin:
+      print("Would you like to save this info? ")
+      print("THIS IS NOT SECURE and will allow anyone with access to this")
+      print("account on your computer the ability to send and recieve ")
+      print("messages via your ZipWhip account")
+      yn = input("Save? y/n ")
+      if yn.upper() == "Y":
+        subprocess.call(["mkdir", "-p", SETTINGS_DIR])
+        with open(SETTINGS_FILE, 'wb') as f:
+          pckl = (u, p)
+          pickle.dump(pckl, f)
+    return s 
+  else:
+    print("Bad username or password.")
+    sys.exit(0)
+
+def console_ui(s):
+  """
+  Basic interactive console UI for zipwhip.  Currently pretty limited.
+  Ncurses UI coming.
+
+  Args:
+    s - String - session key.
+  """
+  print("Select one of the following options:")
+  print("    1. See recent conversations")
+  print("    2. Send a text")
+  print(" ")
+  response = input("selection: ")
+  if int(response) == 1:
+    cl = zwc.conversation_list(s)
+    print("New? | From:      | Msg: ")
+    for k,v in cl.items():
+      if k == 'response':
+        for i, d in enumerate(v):
+          # d looks like
+          # { 'class' : 'java class name'
+          #   'bcc' :
+          #   'lastUpdated' : (timestamp)
+          #   'address' : 'ptn:/phone_number'
+          #   ...
+          #   'lastContactMobileNumber' : 'phone_number'
+          #   'new' : True/False
+          #   'lastMessageBody' : 'msg_body'
+          if d.get('new'):
+            star = '*'
+          else:
+            star = ' ' 
+          lastMsg = d.get('lastMessageBody').replace('\n', ' ')
+          num = d.get('lastContactMobileNumber')
+          multiline = False
+          if len(lastMsg) > 60:
+            multiline = True
+            words = lastMsg.split()
+            num_lines = int((len(lastMsg)/60))+2
+            lines = [" "]*num_lines
+            line = 0
+            for word in words:
+              if len(lines[line]) + len(word) < 60:
+                lines[line] += "%s " % word
+              else:
+                line = line+1
+                lines[line] += "%s " % word
+          if multiline:
+            line = 0
+            print("%4s | %10s | %s" % (star, num, lines[line]))
+            while line < len(lines)-1:
+              line = line+1
+              print("%4s | %10s | %s" % (" ", " ", lines[line]))
+          else:  
+            print("%4s | %10s | %s" % (star, num, lastMsg)) 
+          if i > 1 and i % 19 == 0:
+            yn = input("  <more> ")
+  elif int(response) == 2:
+    num = input("Phone Number to Text: ")
+    msg = input("Message to send: ")
+    print(" ")
+    print("Getting ready to send")
+    print(" ")
+    print("     %s" % msg)
+    print(" ")
+    print("  to: %s" % num)
+    yn = input("Confirm? y/n ")
+    if yn.upper() == "Y":
+      r = zwc.message_send(s, num, msg)
+      if r.get("success"):
+        print("Message sent successfully!")
+             
+if __name__ == "__main__":
+  s = authenticate()
+  console_ui(s) 
