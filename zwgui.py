5d2f7b7099663df77be7b4fa5bd765fc31438ccc
voytek <voytek@trustdarkness.com>
2015-05-22 16:18:27 -0500

    add ability to show recent messages

--- zwgui.py
+++ zwgui.py
@@ -20,6 +20,14 @@ def newmsg(buf):
   win.show_all()
   Gtk.main()
 
+def readmsgs(buf):
+  s = zw_lib.authenticate()
+  recent = zw_lib.get_recent(s, 40)
+  win = CellRendererTextWindow(recent)
+  win.connect("delete-event", Gtk.main_quit)
+  win.show_all()
+  Gtk.main()
+
 def markread(buf):
   s = zw_lib.authenticate()
   zw_lib.show_recent(s, 40, False, True, False)
@@ -75,6 +83,44 @@ def sendMessage(num, msg):
   n = notify2.Notification("Zipwhip", r, "notification-message-im")
   n.show()
 
+class CellRendererTextWindow(Gtk.Window):
+
+    def __init__(self, listitems):
+        Gtk.ScrolledWindow.__init__(self, title="CellRendererText Example")
+        self.set_default_size(500, 600)
+        self.set_position(Gtk.WindowPosition.CENTER)
+        self.set_vexpand(True)
+        liststore = Gtk.ListStore(str, str)
+        for row in listitems:
+          num = row[3]
+          msg = row[4].strip()
+          liststore.append([num, msg])
+
+        treeview = Gtk.TreeView(model=liststore)
+        treeview.set_rules_hint( True )
+        renderer_text = Gtk.CellRendererText()
+        column_text = Gtk.TreeViewColumn("From:", renderer_text, text=0)
+
+        treeview.append_column(column_text)
+
+        renderer_editabletext = Gtk.CellRendererText()
+        renderer_editabletext.set_property("wrap_mode", 2)
+        renderer_editabletext.set_property("wrap_width", 300)
+
+        column_editabletext = Gtk.TreeViewColumn("SMS Message:",
+            renderer_editabletext, text=1)
+        treeview.append_column(column_editabletext)
+
+        renderer_editabletext.connect("edited", self.text_edited)
+        scrolled = Gtk.ScrolledWindow()
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled.add(treeview)
+        self.add(scrolled)
+
+    def text_edited(self, widget, path, text):
+        self.liststore[path][1] = text
+
+
 def background_run():
   while True:
     time.sleep(100)
@@ -87,11 +133,11 @@ def app_main():
 
 if __name__ == "__main__":
   s = zw_lib.authenticate()
-  ind = appindicator.Indicator.new_with_path (
+  ind = appindicator.Indicator.new(
                         "zipwhip-client",
-                        "indicator-demo-icon-normal",
+                        "phone",
                         appindicator.IndicatorCategory.APPLICATION_STATUS,
-                        "./data")
+                       )
   ind.set_status (appindicator.IndicatorStatus.ACTIVE)
   ind.set_attention_icon ("indicator-messages-new")
 
@@ -99,13 +145,15 @@ if __name__ == "__main__":
   menu = Gtk.Menu()
 
   # create some 
-  for i in range(3):
+  for i in range(4):
     if i == 0:
       buf = "Check for new messages"
     elif i == 1:
       buf = "Send a new message"
-    else:
+    elif i == 2:
       buf = "Mark all read"
+    elif i == 3:
+      buf = "View all messages"
 
     menu_items = Gtk.MenuItem(buf)
     if i == 0:
@@ -114,6 +162,8 @@ if __name__ == "__main__":
       menu_items.connect("activate", newmsg)
     elif i == 2:
       menu_items.connect("activate", markread)
+    elif i == 3:
+      menu_items.connect("activate", readmsgs)
 
     menu.append(menu_items)
 
