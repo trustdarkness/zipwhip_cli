038106a7c5972f054de3595d01a429b6143aa56e
voytek <voytek@trustdarkness.com>
2015-05-31 18:33:41 -0500

    removed unused files and added licenses to all files

--- zwgui.py
+++ zwgui.py
@@ -9,6 +9,24 @@ import threading
 import dbus
 import dbus.service
 
+__author__ = "voytek@trustdarkness.com"
+__email__ = "voytek@trustdarkness.com"
+__license__ = "GPL2"
+
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 def menuitem_response(w, buf):
   print(buf)
